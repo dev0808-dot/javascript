b6a26556df6a94fb4bed49f017e0aa49c663b81b
dev0808-dot <204468541+dev0808-dot@users.noreply.github.com>
2025-12-27 12:43:05 +0000

    let,const,var, complete

--- 01_basics/01basics.js
+++ 01_basics/01basics.js
@@ -1,1 +1,17 @@
-console.log("Hello Devran Kumar.")
+// console.log("Hello Devran Kumar.")
+// start storing var and const.
+const accountId = 251656
+let accountEmail = "herpetophile.gmail.com"
+var accountPassword ="ryujin08080"
+/*Do not use var. It does not have scope. */
+accountCity = "Chicago"
+ 
+
+// accountId = 3
+accountEmail = "anything@gmail.com"
+// accountPassword = "08080ryujin"
+accountCity ="Katihar"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountEmail);
+
+
