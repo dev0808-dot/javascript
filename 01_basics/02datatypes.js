28748d4e8fc0416c57986b29967fb9de92262c32
dev0808-dot <204468541+dev0808-dot@users.noreply.github.com>
2025-12-27 13:10:40 +0000

    only first part

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,9 @@
+// "use strict"; // treat all JS code as Newer version.
+
+// let name = "devran kumar"
+// let age = 18
+// let is18plus = true
+
+// console.log(is18plus);
+// console.log(typeof name );
+
