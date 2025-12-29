c36a68fcad8750ac668b36ee0a3da722cab699a3
dev0808-dot <204468541+dev0808-dot@users.noreply.github.com>
2025-12-29 16:33:20 +0000

    operations left

---
+++ 01_basics/03conversionopertion.js
@@ -0,0 +1,15 @@
+// let score = "463abc"
+// console.log(typeof(score));
+
+// let valueinnumber = Number(score) //got converted in number even if the score was mixture of number and string.
+// console.log(typeof(valueinnumber));
+// console.log(valueinnumber); // Output - NaN(Not a Number) but its type is Number.
+
+/*operation */
+
+let value= 9
+// console.log(value%8);
+let str1="devran"
+let str2 =" kumar"
+console.log(str1 + str2);
+
